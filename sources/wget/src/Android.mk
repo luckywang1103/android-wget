0a3cbeb756ff24fe6756643cffe8fe4a95deab44
Frank Wang <lei.wang_15@nxp.com>
2019-03-01 17:24:32 +0800

    change: compile pass in NXP Android_p_auto
    Signed-off-by: Frank Wang <lei.wang_15@nxp.com>

--- sources/wget/src/Android.mk
+++ sources/wget/src/Android.mk
@@ -7,10 +7,27 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-
 LOCAL_MODULE    := wget
+LOCAL_MODULE_TAGS := debug
+LOCAL_SHARED_LIBRARIES += libssl libcrypto libdl
+
 LOCAL_SRC_FILES := alloca.c cmpt.c connect.c convert.c cookies.c ftp-basic.c ftp-ls.c ftp-opie.c ftp.c gnu-md5.c gen-md5.c getopt.c  hash.c host.c html-parse.c html-url.c http.c init.c log.c main.c netrc.c progress.c ptimer.c recur.c res.c retr.c safe-ctype.c snprintf.c spider.c url.c utils.c version.c xmalloc.c
 
-NDK_APP_CPPFLAGS := -DHAVE_CONFIG_H
+LOCAL_CFLAGS += -DHAVE_CONFIG_H -DSYSTEM_WGETRC=\"$(sysconfdir)/wgetrc\" -DLOCALEDIR=\"$(localedir)\"
+#LOCAL_CFLAGS += -D_BSD_SOURCE
+LOCAL_CFLAGS += -D__ANDROID_P
+LOCAL_CFLAGS += -D_U_="__attribute__((unused))"
+LOCAL_CFLAGS += -Wno-format-security
+LOCAL_CFLAGS += -Wno-unused-parameter
+LOCAL_CFLAGS += -Wno-format
+LOCAL_CFLAGS += -Wno-int-to-void-pointer-cast
+LOCAL_CFLAGS += -Wno-implicit-function-declaration
+LOCAL_CFLAGS += -Wno-address-of-packed-member
+LOCAL_CFLAGS += -Wno-sign-compare
+LOCAL_CFLAGS += -Wno-int-to-void-pointer-cast
+LOCAL_CFLAGS += -Wno-incompatible-pointer-types-discards-qualifiers
+LOCAL_CFLAGS += -Wno-error
+LOCAL_CFLAGS += -Wno-int-conversion
+
 
 include $(BUILD_EXECUTABLE)
